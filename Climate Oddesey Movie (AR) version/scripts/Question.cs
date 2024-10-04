66cebea67cbc511b50671b55d9a54a9fb2d9196c
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-10-04 19:42:48 +0600

    Delete Climate Oddesey Movie (AR) version directory

--- Climate Oddesey Movie (AR) version/scripts/Question.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class QuizQuestion
-{
-    public string text; // Question text
-    public string[] options; // Array of 4 options
-    public int correctAnswerIndex; // Index of the correct answer (0-3)
-}
